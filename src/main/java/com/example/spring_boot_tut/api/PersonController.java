42830282fd6113e5a31faad1dead47af4caa16ac
Khoa Hoang <whoskhoahoang@gmail.com>
2020-06-13 21:13:13 +0900

    Define API endpoint for a POST
    Also Mavenized the project

--- src/main/java/com/example/spring_boot_tut/api/PersonController.java
+++ src/main/java/com/example/spring_boot_tut/api/PersonController.java
@@ -3,11 +3,18 @@ package com.example.spring_boot_tut.api;
 import com.example.spring_boot_tut.model.Person;
 import com.example.spring_boot_tut.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 // It is customary to call the class for
 // the API a "controller".
-// TODO: Use the annotaiton that establishes this class
-//       as a REST controller
+
+// @RequestMapping defines the URL for this API
+// @RestController establishes this class as a REST controller
+@RequestMapping("api/v1/person")
+@RestController
 public class PersonController {
 
   private final PersonService personService;
@@ -17,7 +24,11 @@ public class PersonController {
     this.personService = personService;
   }
 
-  public void addPerson(Person person) {
+  @PostMapping
+  public void addPerson(@RequestBody Person person) {
+    // @RequestBody says that we want to take the body of the
+    // incoming HTTP request and put that inside of this person
+    // object
     personService.addPerson(person);
   }
 }
--- src/main/java/com/example/spring_boot_tut/model/Person.java
+++ src/main/java/com/example/spring_boot_tut/model/Person.java
@@ -1,12 +1,17 @@
 package com.example.spring_boot_tut.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.UUID;
 
 public class Person {
   private final UUID id;
   private final String name;
 
-  public Person(UUID id, String name) {
+  // Add the @JsonProperty annotation so that Spring
+  // can interpret JSON requests as Java objects (i.e.,
+  // POJOs [Plain Old Java Objects?]?)
+  public Person(@JsonProperty("id") UUID id,
+                @JsonProperty("name") String name) {
     this.id = id;
     this.name = name;
   }
